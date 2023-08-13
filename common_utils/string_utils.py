c47d54b3b2ef43e6486150f5936b2cf2afa1196e
cwadven <cwadven@naver.com>
2023-08-13 19:53:59 +0900

    Feat: Basic setting
    - basic setting

---
+++ common_utils/string_utils.py
@@ -0,0 +1,30 @@
+import random
+
+import string
+from typing import Sequence, List, Union
+
+
+def generate_random_string_digits(length: int = 4) -> str:
+    return ''.join(random.choice(string.digits) for _ in range(length))
+
+
+def get_filtered_by_startswith_text_and_convert_to_standards(startswith_text: str, keys: Sequence, is_integer=False) -> List[Union[str, int]]:
+    """
+    Filters keys that start with a specific text from an iterable type,
+    and converts the values of specific portions of the keys to integers based on the provided condition.
+
+    [ Example ]
+    If startswith_text is 'home_popup_modal_':
+    Input: ['home_popup_modal_1', 'home_popup_modal_2', 'home_popup_modal_3', 'home_popup_modal_4', 'k_popup_modal_10']
+    Output (if is_integer=True): [1, 2, 3, 4]
+    Output (if is_integer=False): ['1', '2', '3', '4']
+
+    :param startswith_text: The text that the keys should start with for filtering.
+    :param keys: An iterable containing keys to be filtered and processed.
+    :param is_integer: Determines whether the extracted values should be converted to integers.
+    :return: A list containing filtered and processed values based on the specified conditions.
+    """
+    return [
+        int(key.replace(startswith_text, '')) if is_integer else key.replace(startswith_text, '')
+        for key in keys if key.startswith(startswith_text)
+    ]
---
+++ common_utils/tests/string_utils_tests.py
@@ -0,0 +1,65 @@
+from unittest import TestCase
+from unittest.mock import patch
+
+from common_utils.string_utils import (
+    generate_random_string_digits,
+    get_filtered_by_startswith_text_and_convert_to_standards,
+)
+
+
+@patch("common_utils.string_utils.random.choice")
+class TestStringUtils(TestCase):
+    def test_generate_random_string_digits_default_length(self, mock_choice):
+        # Given: Mocking random
+        mock_choice.return_value = "1"
+
+        # When:
+        result = generate_random_string_digits()
+
+        # Then:
+        self.assertEqual(len(result), 4)
+        self.assertEqual(result, "1111")
+
+    def test_generate_random_string_digits_custom_length(self, mock_choice):
+        # Given: Mocking random
+        mock_choice.return_value = "1"
+        length = 6
+
+        # When:
+        result = generate_random_string_digits(length)
+
+        # Then:
+        self.assertEqual(len(result), length)
+        self.assertEqual(result, "111111")
+
+    def test_generate_random_string_digits_zero_length(self, mock_choice):
+        # Given: Mocking random
+        mock_choice.return_value = "1"
+
+        # When:
+        result = generate_random_string_digits(0)
+
+        # Then:
+        self.assertEqual(result, '')
+
+
+class TestFilteredConversion(TestCase):
+    def test_get_filtered_by_startswith_text_with_is_integer_true(self):
+        # Given:
+        input_keys = ['home_popup_modal_1', 'home_popup_modal_2', 'home_popup_modal_3', 'home_popup_modal_4', 'k_popup_modal_10']
+
+        # When: is_integer with True
+        result = get_filtered_by_startswith_text_and_convert_to_standards('home_popup_modal_', input_keys, is_integer=True)
+
+        # Then:
+        self.assertEqual(result, [1, 2, 3, 4])
+
+    def test_get_filtered_by_startswith_text_with_is_integer_false(self):
+        # Given:
+        input_keys = ['home_popup_modal_1', 'home_popup_modal_2', 'home_popup_modal_3', 'home_popup_modal_4', 'k_popup_modal_10']
+
+        # When: is_integer with False
+        result = get_filtered_by_startswith_text_and_convert_to_standards('home_popup_modal_', input_keys, is_integer=False)
+
+        # Then:
+        self.assertEqual(result, ['1', '2', '3', '4'])
