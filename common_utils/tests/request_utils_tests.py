7aa2d25724dd95322257a04013fccd6c41052315
cwadven <cwadven@naver.com>
2023-08-13 20:43:44 +0900

    Feat: Add testcase for request_utils
    - Add testcase for request_utils

---
+++ common_utils/tests/request_utils_tests.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+from unittest.mock import Mock
+
+from common_utils.request_utils import get_request_ip
+
+
+class TestGetRequestIP(TestCase):
+    def test_with_x_forwarded_for(self):
+        # Given:
+        request = Mock()
+        request.META = {'HTTP_X_FORWARDED_FOR': '1.2.3.4'}
+
+        # When:
+        result = get_request_ip(request)
+
+        # Then:
+        self.assertEqual(result, '1.2.3.4')
+
+    def test_without_x_forwarded_for(self):
+        # Given:
+        request = Mock()
+        request.META = {'REMOTE_ADDR': '9.10.11.12'}
+
+        # When:
+        result = get_request_ip(request)
+
+        # Then:
+        self.assertEqual(result, '9.10.11.12')
