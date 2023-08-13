32b26f714dd0c4a9fdf7ae61b5f0bdb032d8c435
cwadven <cwadven@naver.com>
2023-08-13 22:28:05 +0900

    Feat: Test Mangers
    - Test Mangers

---
+++ member/managers.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import UserManager
+
+from member.consts import (
+    SocialLoginModuleSelector,
+    MemberStatusEnum,
+    MemberTypeEnum,
+)
+from member.utils.social_utils import SocialLoginHandler
+
+
+class MemberManager(UserManager):
+    def get_or_create_member_by_token(self, token: str, provider: int) -> tuple:
+        from common_utils.string_utils import generate_random_string_digits
+
+        data = SocialLoginHandler(
+            SocialLoginModuleSelector(int(provider)).selector()
+        ).validate(token)
+
+        return self.get_or_create(
+            username=data['id'],
+            member_provider_id=provider,
+            defaults={
+                'member_type_id': MemberTypeEnum.NORMAL_MEMBER.value,
+                'member_status_id': MemberStatusEnum.NORMAL_MEMBER.value,
+                'email': data['email'] or '',
+                'nickname': data['nickname'] or f'Random{generate_random_string_digits(5)}',
+            }
+        )
