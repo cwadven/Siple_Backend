257ff00cc02f219122f066a5a9fed7538eb2ce85
cwadven <cwadven@naver.com>
2023-09-21 02:06:18 +0900

    Feat: Fabric Set env
    - Fabric Set env

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,44 +1,43 @@
-from .base import * # noqa
+from .base import *  # noqa
 
-from .ENV import env_production
+import json
 
 
-SECRET_KEY = env_production['SECRET_KEY']
-KAKAO_API_KEY = env_production['KAKAO_API_KEY']
-KAKAO_SECRET_KEY = env_production['KAKAO_SECRET_KEY']
-NAVER_API_KEY = env_production['NAVER_API_KEY']
-NAVER_SECRET_KEY = env_production['NAVER_SECRET_KEY']
-GOOGLE_CLIENT_ID = env_production['GOOGLE_CLIENT_ID']
-GOOGLE_SECRET_KEY = env_production['GOOGLE_SECRET_KEY']
-GOOGLE_REDIRECT_URL = env_production['GOOGLE_REDIRECT_URL']
+with open(Path(BASE_DIR) / '.env', 'r') as file:  # noqa
+    data = json.load(file)
 
-AWS_IAM_ACCESS_KEY = env_production['AWS_IAM_ACCESS_KEY']
-AWS_IAM_SECRET_ACCESS_KEY = env_production['AWS_IAM_SECRET_ACCESS_KEY']
-AWS_S3_BUCKET_NAME = env_production['AWS_S3_BUCKET_NAME']
 
-EMAIL_HOST_USER = env_production['EMAIL_HOST_USER']
-EMAIL_HOST_PASSWORD = env_production['EMAIL_HOST_PASSWORD']
+SECRET_KEY = data['SECRET_KEY']
+KAKAO_API_KEY = data['KAKAO_API_KEY']
+KAKAO_SECRET_KEY = data['KAKAO_SECRET_KEY']
+NAVER_API_KEY = data['NAVER_API_KEY']
+NAVER_SECRET_KEY = data['NAVER_SECRET_KEY']
+GOOGLE_CLIENT_ID = data['GOOGLE_CLIENT_ID']
+GOOGLE_SECRET_KEY = data['GOOGLE_SECRET_KEY']
+GOOGLE_REDIRECT_URL = data['GOOGLE_REDIRECT_URL']
+
+AWS_IAM_ACCESS_KEY = data['AWS_IAM_ACCESS_KEY']
+AWS_IAM_SECRET_ACCESS_KEY = data['AWS_IAM_SECRET_ACCESS_KEY']
+AWS_S3_BUCKET_NAME = data['AWS_S3_BUCKET_NAME']
+
+EMAIL_HOST_USER = data['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = data['EMAIL_HOST_PASSWORD']
 
 DATABASES = {
-    'default': env_production['default']
+    'default': data['DATABASE']
 }
 
 # CacheOps
-CACHEOPS_REDIS = env_production['CACHEOPS_REDIS']
+CACHEOPS_REDIS = data['CACHEOPS_REDIS']
 CACHEOPS_DEFAULTS = {
     'timeout': 60 * 15
 }
-CACHEOPS = {
-    # 'common.*': {'ops': 'all', 'timeout': 60*15},
-    # 'banner.*': {'ops': 'all', 'timeout': 60*15},
-    # 'community.*': {'ops': 'all', 'timeout': 60*15},
-    # 'tag.*': {'ops': 'all', 'timeout': 60*15},
-}
+CACHEOPS = {}
 
 # CELERY SETTINGS
 timezone = 'Asia/Seoul'
-CELERY_BROKER_URL = 'redis://localhost:6379/1'
-result_backend = 'redis://localhost:6379/1'
+CELERY_BROKER_URL = data['CELERY_BROKER_URL']
+result_backend = data['result_backend']
 accept_content = ["json"]
 task_serializer = "json"
 result_serializer = "json"
@@ -46,7 +45,7 @@ result_serializer = "json"
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://localhost:6379/1',
+        'LOCATION': data['CACHES_LOCATION'],
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
---
+++ fabfile.py
@@ -0,0 +1,70 @@
+import os
+
+from fabric2 import task
+
+
+@task
+def generate_env(c):
+    """
+    Generate local env file
+    """
+    from config.settings.base import BASE_DIR
+    from pathlib import Path
+    import json
+
+    env_file_path = Path(BASE_DIR) / '.env'
+
+    env_local = {
+        'SECRET_KEY': _get_or_set_environment('SECRET_KEY'),
+        'KAKAO_API_KEY': _get_or_set_environment('KAKAO_API_KEY'),
+        'KAKAO_SECRET_KEY': _get_or_set_environment('KAKAO_SECRET_KEY'),
+        'NAVER_API_KEY': _get_or_set_environment('NAVER_API_KEY'),
+        'NAVER_SECRET_KEY': _get_or_set_environment('NAVER_SECRET_KEY'),
+        'GOOGLE_CLIENT_ID': _get_or_set_environment('GOOGLE_CLIENT_ID'),
+        'GOOGLE_SECRET_KEY': _get_or_set_environment('GOOGLE_SECRET_KEY'),
+        'GOOGLE_REDIRECT_URL': _get_or_set_environment('GOOGLE_REDIRECT_URL'),
+        'CHANNEL_LAYERS': {
+            'default': {
+                'BACKEND': 'channels_redis.core.RedisChannelLayer',
+                'CONFIG': {
+                    'hosts': [(_get_or_set_environment('CHANNEL_HOST'), int(_get_or_set_environment('CHANNEL_PORT')))],
+                },
+            },
+        },
+        'CACHEOPS_REDIS': {
+            'host': _get_or_set_environment('CACHEOPS_REDIS_HOST'),
+            'port': int(_get_or_set_environment('CACHEOPS_REDIS_PORT')),
+            'db': int(_get_or_set_environment('CACHEOPS_REDIS_DB')),
+        },
+        'CACHES_LOCATION': _get_or_set_environment('CACHES_LOCATION'),
+        'DATABASE': {
+            'ENGINE': _get_or_set_environment('DB_ENGINE'),
+            'NAME': _get_or_set_environment('DB_NAME'),
+            'USER': _get_or_set_environment('DB_USER'),
+            'PASSWORD': _get_or_set_environment('DB_PASSWORD'),
+            'HOST': _get_or_set_environment('DB_HOST'),
+            'PORT': _get_or_set_environment('DB_PORT'),
+            'TEST': {
+                'NAME': _get_or_set_environment('DB_TEST_NAME'),
+                'CHARSET': 'utf8',
+                'COLLATION': 'utf8_general_ci',
+            },
+        },
+        'EMAIL_HOST_USER': _get_or_set_environment('EMAIL_HOST_USER'),
+        'EMAIL_HOST_PASSWORD': _get_or_set_environment('EMAIL_HOST_PASSWORD'),
+        'AWS_IAM_ACCESS_KEY': _get_or_set_environment('AWS_IAM_ACCESS_KEY'),
+        'AWS_IAM_SECRET_ACCESS_KEY': _get_or_set_environment('AWS_IAM_SECRET_ACCESS_KEY'),
+        'AWS_S3_BUCKET_NAME': _get_or_set_environment('AWS_S3_BUCKET_NAME'),
+        'AWS_SQS_URL': _get_or_set_environment('AWS_SQS_URL'),
+        'CELERY_BROKER_URL': _get_or_set_environment('CELERY_BROKER_URL'),
+        'result_backend': _get_or_set_environment('result_backend'),
+    }
+
+    with open(env_file_path, 'w', encoding='utf-8') as f:
+        json.dump(env_local, f, ensure_ascii=False, indent=4)
+
+
+def _get_or_set_environment(environment_key):
+    if os.environ.get(environment_key) is None:
+        return input(f'Input {environment_key}: ')
+    return os.environ[environment_key]
