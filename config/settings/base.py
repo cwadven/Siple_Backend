f7569d7f0459188cb400a186f2ec526beeb7ad30
cwadven <cwadven@naver.com>
2023-09-23 22:10:38 +0900

    Feat: Set SMTP Google
    - Set SMTP Google

--- config/settings/base.py
+++ config/settings/base.py
@@ -136,6 +136,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
