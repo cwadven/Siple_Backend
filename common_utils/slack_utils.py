9c561ffd30365b98bec5afbbe5208d2832d60952
cwadven <cwadven@naver.com>
2023-09-29 22:52:51 +0900

    Feat: Slack Message post
    Slack Message post

---
+++ common_utils/slack_utils.py
@@ -0,0 +1,9 @@
+import json
+import requests
+
+
+def notify_slack_simple_text(channel_url: str, text: str):
+    requests.post(
+        url=channel_url,
+        data=json.dumps({'text': text}),
+    )
