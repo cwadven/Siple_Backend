ddf00bdeeadcd6c638147f2e5d6328ac602b4e9c
cwadven <cwadven@naver.com>
2023-11-25 22:42:42 +0900

    Feat: Add test_get_jwt_refresh_token
    - Add test_get_jwt_refresh_token

--- common/common_utils/tests/test_token_utils.py
+++ common/common_utils/tests/test_token_utils.py
@@ -1,7 +1,13 @@
+from datetime import datetime
 from django.test import TestCase
 from unittest.mock import patch
 
-from common.common_utils.token_utils import get_jwt_login_token
+from freezegun import freeze_time
+
+from common.common_utils.token_utils import (
+    get_jwt_login_token,
+    get_jwt_refresh_token,
+)
 from member.models import Member
 
 
@@ -9,8 +15,8 @@ class TestGetJWTLoginToken(TestCase):
     def setUp(self):
         self.member = Member.objects.all().first()
 
-    @patch('common_utils.token_utils.jwt_payload_handler')
-    @patch('common_utils.token_utils.jwt_encode_handler')
+    @patch('common.common_utils.token_utils.jwt_payload_handler')
+    @patch('common.common_utils.token_utils.jwt_encode_handler')
     def test_get_jwt_login_token(self, mock_jwt_encode_handler, mock_jwt_payload_handler):
         # Given:
         # When:
@@ -19,3 +25,21 @@ class TestGetJWTLoginToken(TestCase):
         # Then: Ensure the function executes and returns the expected result
         mock_jwt_payload_handler.assert_called_once_with(self.member)
         mock_jwt_encode_handler.assert_called_once_with(mock_jwt_payload_handler.return_value)
+
+
+class TestGetJWTRefreshToken(TestCase):
+    def setUp(self):
+        self.member = Member.objects.all().first()
+
+    @freeze_time('2020-01-01 00:00:00')
+    @patch('common.common_utils.token_utils.jwt_encode_handler')
+    def test_get_jwt_refresh_token(self, mock_jwt_encode_handler):
+        # Given:
+        # When:
+        get_jwt_refresh_token(self.member)
+
+        # Then: Ensure the function executes and returns the expected result
+        mock_jwt_encode_handler.assert_called_once_with({
+            'user_id': self.member.id,
+            'exp': datetime(2020, 1, 8)
+        })
--- common/common_utils/token_utils.py
+++ common/common_utils/token_utils.py
@@ -1,3 +1,4 @@
+from datetime import timedelta, datetime
 from rest_framework_jwt.settings import api_settings
 
 from member.models import Member
@@ -11,3 +12,12 @@ def get_jwt_login_token(member: Member) -> str:
     payload = jwt_payload_handler(member)
     token = jwt_encode_handler(payload)
     return token
+
+
+def get_jwt_refresh_token(member: Member) -> str:
+    refresh_expiration = timedelta(days=7)
+    refresh_token = jwt_encode_handler({
+        'user_id': member.id,
+        'exp': datetime.utcnow() + refresh_expiration
+    })
+    return refresh_token
