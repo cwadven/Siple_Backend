6a1e13b643e506d068d11a34d6edee1d81799bce
cwadven <cwadven@naver.com>
2023-12-29 21:50:04 +0900

    Feat: 블랙 리스트 문구
    - BlackListSection
    - BlackListWord

---
+++ common/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+from common.models import (
+    BlackListWord,
+    BlackListSection,
+)
+
+
+class BlackListSectionAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'name',
+    ]
+
+
+class BlackListWordAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'wording',
+        'black_list_section',
+    ]
+
+
+admin.site.register(BlackListSection, BlackListSectionAdmin)
+admin.site.register(BlackListWord, BlackListWordAdmin)
