a21c6d74e01d28e681938c0428c02efb324b5c16
cwadven <cwadven@naver.com>
2024-01-01 12:17:14 +0900

    Feat: Add Cancel rollback given product
    - Add Cancel rollback given product

--- point/exceptions.py
+++ point/exceptions.py
@@ -11,3 +11,15 @@ class NotEnoughGuestPoints(APIException):
         return {
             'message': cls.default_detail,
         }
+
+
+class NotEnoughGuestPointsForCancelOrder(APIException):
+    status_code = 400
+    default_detail = '사용한 포인트가 존재하여 주문 취소를 실패했습니다.'
+    default_code = 'cannot-cancel-order-with-guest-points'
+
+    @classmethod
+    def to_message(cls):
+        return {
+            'message': cls.default_detail,
+        }
