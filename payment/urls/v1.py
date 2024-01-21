0c8bcdd736f23916fbfe0d9dfdce05fb62be565d
cwadven <cwadven@naver.com>
2024-01-21 12:40:51 +0900

    Feat: order_id 를 암호화합니다.
    - 암호화 및 복호화

--- payment/urls/v1.py
+++ payment/urls/v1.py
@@ -16,10 +16,10 @@ app_name = 'payment'
 urlpatterns = [
     path('product/buy/kakao', KakaoPayReadyForBuyProductAPIView.as_view(), name='product_buy'),
     path('product/approve/kakao/<int:order_id>', KakaoPayApproveForBuyProductAPIView.as_view(), name='product_approve'),
-    path('product/cancel/kakao/<int:order_id>', KakaoPayCancelForBuyProductAPIView.as_view(), name='product_cancel'),
-    path('product/fail/kakao/<int:order_id>', KakaoPayFailForBuyProductAPIView.as_view(), name='product_fail'),
+    path('product/cancel/kakao/<str:order_token>', KakaoPayCancelForBuyProductAPIView.as_view(), name='product_cancel'),
+    path('product/fail/kakao/<str:order_token>', KakaoPayFailForBuyProductAPIView.as_view(), name='product_fail'),
 
     path('product/approve/kakao/template/<int:order_id>', approve_give_product_success_by_template, name='product_approve_template'),
-    path('product/cancel/kakao/template/<int:order_id>', approve_give_product_cancel_by_template, name='product_cancel_template'),
-    path('product/fail/kakao/template/<int:order_id>', approve_give_product_fail_by_template, name='product_fail_template'),
+    path('product/cancel/kakao/template/<str:order_token>', approve_give_product_cancel_by_template, name='product_cancel_template'),
+    path('product/fail/kakao/template/<str:order_token>', approve_give_product_fail_by_template, name='product_fail_template'),
 ]
