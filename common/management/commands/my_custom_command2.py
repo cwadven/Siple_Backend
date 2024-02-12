a203a2288ce7900a2e042bee5d718a449f46c439
cwadven <cwadven@naver.com>
2024-02-12 12:16:33 +0900

    Feat: Docker settging
    - Docker settging

--- common/management/commands/my_custom_command2.py
+++ common/management/commands/my_custom_command2.py
@@ -5,5 +5,5 @@ class Command(BaseCommand):
     help = '설명: 이 명령어는 ... 작업을 수행합니다.'
 
     def handle(self, *args, **kwargs):
-        self.stdout.write(self.style.SUCCESS('TEST 2'))
+        self.stdout.write(self.style.SUCCESS('TEST 3'))
         # 여기에 실행할 작업을 추가합니다.
