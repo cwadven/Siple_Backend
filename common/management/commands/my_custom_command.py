ba2edf5d24630708957bd26eb1d26b4313c9dd85
cwadven <cwadven@naver.com>
2024-02-12 11:37:37 +0900

    Feat: Test command
    - CRON docker test

---
+++ common/management/commands/my_custom_command.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = '설명: 이 명령어는 ... 작업을 수행합니다.'
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write(self.style.SUCCESS('TEST 1'))
+        # 여기에 실행할 작업을 추가합니다.
