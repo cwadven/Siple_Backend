953b6883ff77aa3ca03d1932175ae9c44897b6aa
cwadven <cwadven@naver.com>
2024-02-17 03:25:45 +0900

    Feat: #!/bin/sh 실행하도록 스크립트 수정
    - #!/bin/sh 실행하도록 스크립트 수정

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -4,7 +4,7 @@
 NETWORK_NAME="common_network"
 
 # Search from Docker Network List
-network_exists=$(docker network ls | grep $NETWORK_NAME)
+network_exists=$(docker network ls | grep -w $NETWORK_NAME)
 
 # If the network does not exist, create it
 if [ -z "$network_exists" ]; then
@@ -12,16 +12,15 @@ if [ -z "$network_exists" ]; then
     docker network create $NETWORK_NAME
 fi
 
-
 DATA_COMPOSE_FILE=docker-compose-data.yml
-data_services=("database" "pgadmin" "cache")
+data_services="database pgadmin cache"
 
 # Check if all data services are running
 data_all_services_up=true
 
-for service in "${data_services[@]}"; do
+for service in $data_services; do
     # Check service by 'docker-compose ps'
-    if ! docker-compose -f $DATA_COMPOSE_FILE ps | grep -q "$service"; then
+    if ! docker-compose -f $DATA_COMPOSE_FILE ps | grep -q $service; then
         echo "$service service is not running."
         data_all_services_up=false
         break
@@ -29,23 +28,21 @@ for service in "${data_services[@]}"; do
 done
 
 # If one or more services are not running, build and run
-if [ "$data_all_services_up" = false ]; then
+if [ "$data_all_services_up" = "false" ]; then
     echo "One or more services are not running. Starting build and run..."
     docker-compose -f $DATA_COMPOSE_FILE down
     docker-compose -f $DATA_COMPOSE_FILE up -d --build
 fi
 
-
 WEB_COMPOSE_FILE=docker-compose-web.yml
-web_services=("web_server" "celery_monitor")
-
+web_services="web_server celery_monitor"
 
 # Check if all web services are running
 web_all_services_up=true
 
-for service in "${web_services[@]}"; do
+for service in $web_services; do
     # Check service by 'docker-compose ps'
-    if ! docker-compose -f $WEB_COMPOSE_FILE ps | grep -q "$service"; then
+    if ! docker-compose -f $WEB_COMPOSE_FILE ps | grep -q $service; then
         echo "$service service is not running."
         web_all_services_up=false
         break
@@ -53,26 +50,25 @@ for service in "${web_services[@]}"; do
 done
 
 # If one or more services are not running, build and run
-if [ "$web_all_services_up" = false ]; then
+if [ "$web_all_services_up" = "false" ]; then
     echo "One or more services are not running. Starting build and run..."
     docker-compose -f $WEB_COMPOSE_FILE down
     docker-compose -f $WEB_COMPOSE_FILE up -d --build
 fi
 
-
 # Application Deployment
 BLUE_APP_COMPOSE_FILE=docker-compose-app-blue.yml
 GREEN_APP_COMPOSE_FILE=docker-compose-app-green.yml
 
-blue_app_services=("web_blue" "cron_blue" "celery_blue")
-green_app_services=("web_green" "cron_green" "celery_green")
+blue_app_services="web_blue cron_blue celery_blue"
+green_app_services="web_green cron_green celery_green"
 
 app_blue_services_up=true
 app_green_services_up=true
 
 # Check if all blue app services are running
-for service in "${blue_app_services[@]}"; do
-    if ! docker-compose -f $BLUE_APP_COMPOSE_FILE ps | grep -q "$service"; then
+for service in $blue_app_services; do
+    if ! docker-compose -f $BLUE_APP_COMPOSE_FILE ps | grep -q $service; then
         echo "$service service is not running."
         app_blue_services_up=false
         break
@@ -80,30 +76,31 @@ for service in "${blue_app_services[@]}"; do
 done
 
 # Check if all green app services are running
-for service in "${green_app_services[@]}"; do
-    if ! docker-compose -f $GREEN_APP_COMPOSE_FILE ps | grep -q "$service"; then
+for service in $green_app_services; do
+    if ! docker-compose -f $GREEN_APP_COMPOSE_FILE ps | grep -q $service; then
         echo "$service service is not running."
         app_green_services_up=false
         break
     fi
 done
 
-if [ "$app_blue_services_up" = false ] && [ "$app_green_services_up" = false ]; then
+# Deployment logic
+if [ "$app_blue_services_up" = "false" ] && [ "$app_green_services_up" = "false" ]; then
     echo "Both blue and green servers are not running. Starting build blue and run..."
     docker-compose -f $BLUE_APP_COMPOSE_FILE up -d
-    docker exec web_server /bin/sh /etc/nginx/conf.d/nginx_update_blue.sh
+    docker exec web_server /bin/sh -c '/etc/nginx/conf.d/nginx_update_blue.sh'
     echo "Nginx setting to blue environment"
-elif [ "$app_blue_services_up" = true ]; then
+elif [ "$app_blue_services_up" = "true" ]; then
     echo "Deploying green server."
     docker-compose -f $GREEN_APP_COMPOSE_FILE up -d
-    docker exec web_server /bin/sh /etc/nginx/conf.d/nginx_update_green.sh
+    docker exec web_server /bin/sh -c '/etc/nginx/conf.d/nginx_update_green.sh'
     echo "Nginx setting to green environment."
     docker-compose -f $BLUE_APP_COMPOSE_FILE down
     echo "Remove blue environment."
-elif [ "$app_green_services_up" = true ]; then
+elif [ "$app_green_services_up" = "true" ]; then
     echo "Deploying blue server."
     docker-compose -f $BLUE_APP_COMPOSE_FILE up -d
-    docker exec web_server /bin/sh /etc/nginx/conf.d/nginx_update_blue.sh
+    docker exec web_server /bin/sh -c '/etc/nginx/conf.d/nginx_update_blue.sh'
     echo "Nginx setting to blue environment."
     docker-compose -f $GREEN_APP_COMPOSE_FILE down
     echo "Remove green environment."
