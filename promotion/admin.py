eca8126db992295dfa3c26aaf98c417419fc35f0
cwadven <cwadven@naver.com>
2024-02-25 16:42:28 +0900

    Feat: Add admin
    - Add admin

--- promotion/admin.py
+++ promotion/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
-# Register your models here.
+from promotion.models import PromotionRule, PromotionTag, Banner
+
+
+class PromotionRuleAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'description',
+        'displayable',
+        'display_start_time',
+        'display_end_time',
+        'action_page',
+        'target_url',
+    ]
+
+
+class PromotionTagAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'name',
+    ]
+
+
+class BannerAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'promotion_rule',
+        'title',
+        'title_font_color',
+        'description',
+        'description_font_color',
+        'background_color',
+    ]
+
+
+admin.site.register(PromotionRule, PromotionRuleAdmin)
+admin.site.register(PromotionTag, PromotionTagAdmin)
+admin.site.register(Banner, BannerAdmin)
