ef25af2fa10d7dd162457a54bd0763078edf9f11
cwadven <cwadven@naver.com>
2024-03-04 18:21:07 +0900

    Feat: Make as return Queryset
    - Make as return Queryset

--- promotion/services.py
+++ promotion/services.py
@@ -1,24 +1,23 @@
-from typing import List
-
-from django.db.models import Q
+from django.db.models import (
+    Q,
+    QuerySet,
+)
 from django.utils import timezone
 from promotion.consts import BannerTargetLayer
 from promotion.models import Banner
 
 
-def get_active_banners(target_layer: BannerTargetLayer, now=None) -> List[Banner]:
+def get_active_banners(target_layer: BannerTargetLayer, now=None) -> QuerySet[Banner]:
     if now is None:
         now = timezone.now()
 
-    return list(
-        Banner.objects.select_related(
-            'promotion_rule'
-        ).filter(
-            Q(promotion_rule__displayable=True),
-            Q(target_layer=target_layer.value),
-            Q(promotion_rule__display_start_time__lte=now) | Q(promotion_rule__display_start_time__isnull=True),
-            Q(promotion_rule__display_end_time__gte=now) | Q(promotion_rule__display_end_time__isnull=True),
-        ).prefetch_related(
-            'tags'
-        )
+    return Banner.objects.select_related(
+        'promotion_rule'
+    ).filter(
+        Q(promotion_rule__displayable=True),
+        Q(target_layer=target_layer.value),
+        Q(promotion_rule__display_start_time__lte=now) | Q(promotion_rule__display_start_time__isnull=True),
+        Q(promotion_rule__display_end_time__gte=now) | Q(promotion_rule__display_end_time__isnull=True),
+    ).prefetch_related(
+        'tags'
     )
