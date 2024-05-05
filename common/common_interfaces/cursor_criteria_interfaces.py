7fc758aabbd5be4a2982481f6cf6867dd8aa8c5c
cwadven <cwadven@naver.com>
2024-05-05 21:18:35 +0900

    Feat: get_encoded_base64_cursor_data 추가
    - get_encoded_base64_cursor_data 추가

--- common/common_interfaces/cursor_criteria_interfaces.py
+++ common/common_interfaces/cursor_criteria_interfaces.py
@@ -2,6 +2,7 @@ from abc import (
     ABC,
     abstractmethod,
 )
+from typing import Any
 
 
 class CursorCriteria(ABC):
@@ -9,4 +10,8 @@ class CursorCriteria(ABC):
     @abstractmethod
     def is_valid_decoded_cursor(cls, decoded_cursor: dict) -> bool:
         """Define this method to validate decoded cursors in subclasses."""
+        pass
+
+    def get_encoded_base64_cursor_data(self, data: Any) -> str:
+        """Define this method to get cursor data in subclasses."""
         pass
