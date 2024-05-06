87c5c1b38ecc747bf4acd7509d3284da4f1b40d5
cwadven <cwadven@naver.com>
2024-05-06 19:53:52 +0900

    Feat: Set image as nullable
    - image

--- project/dtos/model_dtos.py
+++ project/dtos/model_dtos.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import (
     BaseModel,
     Field,
@@ -12,5 +14,5 @@ class ProjectListItem(BaseModel):
     experience: str = Field(..., description='프로젝트 들어올 수 있는 경력 수준')
     engagement_level: str = Field(..., description='프로젝트 몰입도 수준')
     current_recruit_status: str = Field(..., description='현재 모집 상태')
-    image: str = Field(..., description='프로젝트 이미지')
+    image: Optional[str] = Field(None, description='프로젝트 이미지')
     is_bookmarked: bool = Field(..., description='북마크 여부')
