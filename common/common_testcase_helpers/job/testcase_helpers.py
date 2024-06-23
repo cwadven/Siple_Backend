e321d7f2ac89f55167727d9ab0a5dbcb25f5acea
cwadven <cwadven@naver.com>
2024-06-23 13:20:35 +0900

    Feat: Create create_job_category_for_testcase
    - create_job_category_for_testcase

--- common/common_testcase_helpers/job/testcase_helpers.py
+++ common/common_testcase_helpers/job/testcase_helpers.py
@@ -1,8 +1,16 @@
-from job.models import Job
+from job.models import Job, JobCategory
 
 
 def create_job_for_testcase(name: str) -> Job:
     return Job.objects.create(
+        display_name=name,
+        name=name,
+        description=name,
+    )
+
+
+def create_job_category_for_testcase(name: str) -> JobCategory:
+    return JobCategory.objects.create(
         display_name=name,
         name=name,
         description=name,
