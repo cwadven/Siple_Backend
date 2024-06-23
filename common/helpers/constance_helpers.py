052c0a9c1d1bd0e638183767f79d3d782387740e
cwadven <cwadven@naver.com>
2024-06-23 20:31:25 +0900

    Feat: Create ConstanceProjectCategoryTypeHelper
    - Create ConstanceProjectCategoryTypeHelper

--- common/helpers/constance_helpers.py
+++ common/helpers/constance_helpers.py
@@ -1,6 +1,8 @@
 from common.dtos.helper_dtos import ConstanceType
 from job.models import JobCategory
 from job.services.project_job_services import get_active_job_categories
+from project.models import ProjectCategory
+from project.services.project_services import get_active_project_categories
 
 
 class ConstanceTypeHelper(object):
@@ -24,6 +26,23 @@ class ConstanceJobCategoryTypeHelper(ConstanceTypeHelper):
         ]
 
 
+class ConstanceProjectCategoryTypeHelper(ConstanceTypeHelper):
+    @staticmethod
+    def get_project_categories() -> list[ProjectCategory]:
+        return get_active_project_categories()
+
+    def get_constance_types(self) -> list[ConstanceType]:
+        return [
+            ConstanceType(
+                id=category.id,
+                name=category.name,
+                display_name=category.display_name,
+            )
+            for category in self.get_project_categories()
+        ]
+
+
 CONSTANCE_TYPE_HELPER_MAPPER = {
     'job-category': ConstanceJobCategoryTypeHelper(),
+    'project-category': ConstanceProjectCategoryTypeHelper(),
 }
--- common/helpers/tests/test_constance_helpers.py
+++ common/helpers/tests/test_constance_helpers.py
@@ -1,10 +1,12 @@
 from unittest.mock import patch
 
 from common.common_testcase_helpers.job.testcase_helpers import create_job_category_for_testcase
+from common.common_testcase_helpers.project.testcase_helpers import create_project_category_for_testcase
 from common.dtos.helper_dtos import ConstanceType
 from common.helpers.constance_helpers import (
     CONSTANCE_TYPE_HELPER_MAPPER,
     ConstanceJobCategoryTypeHelper,
+    ConstanceProjectCategoryTypeHelper,
     ConstanceTypeHelper,
 )
 from django.test import TestCase
@@ -54,11 +56,55 @@ class ConstanceJobCategoryTypeHelperTest(TestCase):
         mock_get_job_categories.assert_called_once()
 
 
+class ConstanceProjectCategoryTypeHelperTest(TestCase):
+    def setUp(self):
+        self.project_category1 = create_project_category_for_testcase('project_category1')
+        self.project_category2 = create_project_category_for_testcase('project_category2')
+
+    @patch('common.helpers.constance_helpers.get_active_project_categories')
+    def test_get_project_categories(self, mock_get_active_project_categories):
+        # Given: Set up the test data
+        mock_get_active_project_categories.return_value = [self.project_category1, self.project_category2]
+
+        # When: Call the function
+        project_categories = ConstanceProjectCategoryTypeHelper().get_project_categories()
+
+        # Then: Assert the result
+        self.assertEqual(project_categories, [self.project_category1, self.project_category2])
+        mock_get_active_project_categories.assert_called_once()
+
+    @patch('common.helpers.constance_helpers.ConstanceProjectCategoryTypeHelper.get_project_categories')
+    def test_get_constance_types(self, mock_get_project_categories):
+        # Given: Set up the test data
+        mock_get_project_categories.return_value = [self.project_category1, self.project_category2]
+
+        # When: Call the function
+        constance_types = ConstanceProjectCategoryTypeHelper().get_constance_types()
+
+        # Then: Assert the result
+        self.assertEqual(
+            constance_types,
+            [
+                ConstanceType(id=self.project_category1.id, name=self.project_category1.name, display_name=self.project_category1.display_name),
+                ConstanceType(id=self.project_category2.id, name=self.project_category2.name, display_name=self.project_category2.display_name),
+            ],
+        )
+        mock_get_project_categories.assert_called_once()
+
+
 class ConstanceTypeMapperTest(TestCase):
-    def test_constance_type_mapper(self):
+    def test_constance_type_mapper_job_category(self):
         # Given:
         # When: Call
         constance_type_helper = CONSTANCE_TYPE_HELPER_MAPPER['job-category']
 
         # Then: Assert the result
         self.assertIsInstance(constance_type_helper, ConstanceJobCategoryTypeHelper)
+
+    def test_constance_type_mapper_project_category(self):
+        # Given:
+        # When: Call
+        constance_type_helper = CONSTANCE_TYPE_HELPER_MAPPER['project-category']
+
+        # Then: Assert the result
+        self.assertIsInstance(constance_type_helper, ConstanceProjectCategoryTypeHelper)
