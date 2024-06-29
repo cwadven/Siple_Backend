853d3fb10db4b58dd24207047a97f0b8772c8220
cwadven <cwadven@naver.com>
2024-06-29 19:07:44 +0900

    Feat: Add Input should be a valid integer
    - Add Input should be a valid integer

--- common/common_pydantic/custom_error_messages.py
+++ common/common_pydantic/custom_error_messages.py
@@ -3,4 +3,5 @@ ERROR_MESSAGE_CONVERSION = {
     'Input should be a valid string': '문자열 형식으로 입력해주세요.',
     'unable to parse string as an integer': '숫자 형식으로 입력해주세요.',
     'Input should be a valid list': '유효한 데이터를 입력해주세요.',
+    'Input should be a valid integer': '숫자 형식으로 입력해주세요.',
 }
