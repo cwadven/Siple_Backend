26d4f88462c5f2f8fe01e4c5ed4c1e0f4fcbdf0f
cwadven <cwadven@naver.com>
2024-07-06 17:08:46 +0900

    Feat: No need field delete
    - No need field delete

--- member/dtos/model_dtos.py
+++ member/dtos/model_dtos.py
@@ -1,6 +1,5 @@
 from datetime import (
     date,
-    datetime,
 )
 from typing import (
     Optional,
@@ -16,4 +15,3 @@ class JobExperience(BaseModel):
     job_id: int = Field(description='Job id')
     start_date: date = Field(description='직무 시작인')
     end_date: Optional[date] = Field(description='직무 종료일')
-    created_at: datetime = Field(description='bulk create 순서 및 조회를 위해')
