10fe225fbd9754bcef3e17c4f514a501f082dc27
cwadven <cwadven@naver.com>
2024-07-06 20:36:23 +0900

    Feat: Add select_related
    - Add select_related

--- project/services/project_services.py
+++ project/services/project_services.py
@@ -260,7 +260,7 @@ def get_active_project_categories() -> List[ProjectCategory]:
 
 def get_active_project(project_id: int) -> Optional[Project]:
     try:
-        return Project.objects.get(
+        return Project.objects.select_related('category').get(
             id=project_id,
             is_deleted=False,
         )
