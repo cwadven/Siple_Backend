1e224a9f46bbf5d57df10b1c655ccca187635e61
cwadven <cwadven@naver.com>
2024-07-06 20:52:00 +0900

    Feat: Add from_recruit_info
    - Add from_recruit_info

--- job/dtos/model_dtos.py
+++ job/dtos/model_dtos.py
@@ -19,3 +19,11 @@ class ProjectJobAvailabilities(BaseModel):
     id: int = Field(..., description='Job Id')
     display_name: str = Field(..., description='Job display name')
     is_available: bool = Field(..., description='Job availability')
+
+    @classmethod
+    def from_recruit_info(cls, recruit_info: ProjectJobRecruitInfo) -> 'ProjectJobAvailabilities':
+        return cls(
+            id=recruit_info.job_id,
+            display_name=recruit_info.job_display_name,
+            is_available=not bool(recruit_info.total_limit) or bool(recruit_info.total_limit > recruit_info.current_recruited),
+        )
