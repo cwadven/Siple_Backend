6cb282b9fe41d5cb32e6271e5e08afe6dbf28ebd
이창우(Changwoo Lee) <changwoo.lee@wesang.com>
2024-09-01 23:26:57 +0900

    Feat: DefaultDict
    - DefaultDict

--- project/services/project_permission_services.py
+++ project/services/project_permission_services.py
@@ -1,16 +1,16 @@
 from collections import defaultdict
-from typing import List
+from typing import List, DefaultDict
 
 from project.consts import ProjectManagementPermissionStatus
 from project.models import ProjectManagementPermission
 
 
-def get_project_permissions(member_id: int, project_ids: List[int]) -> dict[int, List[str]]:
-    if not member_id:
-        return {}
-    if not project_ids:
-        return {}
+def get_project_permissions(member_id: int, project_ids: List[int]) -> DefaultDict[int, List[str]]:
     permissions_by_project_id = defaultdict(list)
+    if not member_id:
+        return permissions_by_project_id
+    if not project_ids:
+        return permissions_by_project_id
     active_project_management_permission_qs = ProjectManagementPermission.objects.filter(
         member_id=member_id,
         project_id__in=project_ids,
